5e50e34288f90887736255ca60b20920d29f8b4f
Ernane Sena <ernane.sena@gmail.com>
2015-09-09 21:07:51 -0300

    Projeto Pronto para ser Refatorado

--- src/main/java/br/com/easoftware/domain/GtinProduto.java
+++ src/main/java/br/com/easoftware/domain/GtinProduto.java
@@ -1,12 +1,41 @@
 package br.com.easoftware.domain;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class GtinProduto {
-	
-	public double calculaValor(Gtin gtin) {
-		return gtin.getPreco() * gtin.getQuantidade();
+
+	public void cadastraProduto(Produto produto) {
+
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("EaSoftwareGTIN");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		tx.begin();
+		em.persist(produto);
+		tx.commit();
+		em.close();
+
 	}
 
-	public double aplicaReducao(Gtin gtin, String unidade) {
+	public void cadastraGtin(Gtin gtin) {
+
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("EaSoftwareGTIN");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		tx.begin();
+		em.persist(gtin);
+		tx.commit();
+		em.close();
+
+	}
+	
+	public double aplicaReducaoPorUnidade(Gtin gtin, String unidade) {
 		double valor = gtin.getPreco() * gtin.getQuantidade();
 		double desconto = 0.0;
 
@@ -22,7 +51,23 @@ public class GtinProduto {
 		if (unidade == "PC") {
 			desconto = 0.02;
 		}
+		valor = valor - (desconto * valor);
+
+		return valor;
+	}
+	
+	public double aplicaReducaoPorUF(Gtin gtin, String uf){
+		double valor = gtin.getPreco() * gtin.getQuantidade();
+		double desconto = 0.017;
+		double resultado;
 		
-		return valor - desconto;
+		if(uf == "PA"){
+			desconto = 0.019;
+		}
+		if(uf == "SP"){
+			desconto = 0.013;
+		}
+		resultado = valor = valor - (desconto * valor);
+		return resultado;
 	}
 }
